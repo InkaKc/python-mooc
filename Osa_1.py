8cc261e63c2aa7cd592de86afc01e3353f33b8e9
Your Name <you@example.com>
2021-03-18 14:50:03 +0200

    tehtävä tehty4

--- Osa_1.py
+++ Osa_1.py
@@ -29,3 +29,27 @@ print(nimi)
 # Kirjoita ratkaisu tähän
 nimi = input("Anna nimesi: ")
 print("!" + nimi + "!" + nimi + "!")
+
+# Kirjoita ratkaisu tähän
+nimi = input("Etunimi: ")
+snimi = input("Sukunimi: ")
+osoite = input("Katuosoite ")
+kaupunki = input("Postinumero ja kaupunki ")
+
+print(nimi + " " + snimi)
+print(osoite)
+print(kaupunki)
+
+# Korjaa ohjelma
+osa1 = input("Anna 1. osa: ")
+osa2 = input("Anna 2. osa: ")
+osa3 = input("Anna 3. osa: ")
+print(osa1 + "-" + osa2 + "-" + osa3 + "!")
+
+# Kirjoita ratkaisu tähän
+nimi = input("Anna nimi: ")
+vuosi = input("Anna syntymävuosi: ")
+
+print(nimi + " " + "on urhea ritari, syntynyt vuonna" + " " + vuosi + ". Eräänä 
+aamuna" + " " + nimi + " " + "heräsi kovaan meluun: lohikäärme lähestyi kylää.
+Vain" + " " + nimi + " " + "voisi pelastaa kylän asukkaat.")
