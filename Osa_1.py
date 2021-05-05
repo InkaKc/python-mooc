ffe979ff6e8172d32c200fc1dab9c90c1b46c679
Your Name <you@example.com>
2021-05-05 19:26:35 +0300

    tehtävä tehty12

--- Osa_1.py
+++ Osa_1.py
@@ -53,3 +53,23 @@ vuosi = input("Anna syntymävuosi: ")
 print(nimi + " " + "on urhea ritari, syntynyt vuonna" + " " + vuosi + ". Eräänä 
 aamuna" + " " + nimi + " " + "heräsi kovaan meluun: lohikäärme lähestyi kylää.
 Vain" + " " + nimi + " " + "voisi pelastaa kylän asukkaat.")
+
+#Välilyönnillä vai ilman
+nimi = "Teppo Testaaja"
+ika = 20
+taito1 = "python"
+taso1 = "aloittelija"
+taito2 = "java"
+taso2 = "veteraani"
+taito3 = "ohjelmointi"
+taso3 = "puoliammattilainen"
+ala = 2000
+yla = 3000
+ika_merkkijono = str(ika)
+
+print("nimeni on", nimi + ", olen", str(ika) + "-vuotias\n") 
+print("taitoihini kuuluvat")
+print(" -", taito1, "(" + taso1 + ")")
+print(" -", taito2, "(" + taso2 + ")")
+print(" -", taito3, "(" + taso3 + ")\n")
+print("haen työtä, josta maksetaan palkkaa", str(ala) + "-" + str(yla), "euroa kuussa")
